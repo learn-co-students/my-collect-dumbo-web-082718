f866ead9f11b98e8019f1f1a4c15966e03b3ea2d
Gavin Soko <sokolofg@gmail.com>
2018-08-17 15:15:36 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
+def my_collect(collection)
+  if block_given?
+    i = 0 
+    myArr = []
+    while i < collection.length
+   myArr << yield(collection[i])
+    i += 1 
+  end
+  myArr
+end
+end
+  
 
