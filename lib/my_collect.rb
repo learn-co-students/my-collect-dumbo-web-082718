caf5b6191c42fcf1f7f8641e0296c0fa6b6767c0
Nasser Sanou <nassersanou23@gmail.com>
2018-08-22 21:58:27 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,21 @@
-# languages = ['ruby', 'javascript', 'python', 'objective-c'] 
 # students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'] 
 
+languages = ['ruby', 'javascript', 'python', 'objective-c'] 
+
 def my_collect(languages)
-  
-  collection.collect do |language| language
-    yield collection[language].capitalize
-  end 
-  new_array 
-end 
+  if block_given? 
+
+	i = 0
+	new_array = []
+	
+  while i < languages.length
+      
+			new_array << yield(languages[i].capitalize)
+			i += 1
+	end
+	new_array
+  else
+   puts " No block was given to me"
+   end 
+end
+puts my_collect(languages)
