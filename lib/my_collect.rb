eb9ab6e309c064461be1d2ede1e34f72156f7c49
Nasser Sanou <nassersanou23@gmail.com>
2018-08-22 21:22:16 +0000

    Each

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
+# languages = ['ruby', 'javascript', 'python', 'objective-c'] 
+# students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'] 
 
-
+def my_collect(languages)
+  
+  collection.collect do |language| language
+    yield collection[language].capitalize
+  end 
+  new_array 
+end 
